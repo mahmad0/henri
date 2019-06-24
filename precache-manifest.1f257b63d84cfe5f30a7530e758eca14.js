cb3be5c54d52a4f32dbf79d5e590703f5933a021
mahmad0 <lejazzman1@gmail.com>
2019-06-24 11:59:01 +0000

    Updates

---
+++ precache-manifest.1f257b63d84cfe5f30a7530e758eca14.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44c7890335fb65b97600",
+    "url": "/henri/static/js/runtime~main.44c78903.js"
+  },
+  {
+    "revision": "da761812f0e419334d0f",
+    "url": "/henri/static/js/main.da761812.chunk.js"
+  },
+  {
+    "revision": "fed5b92daff37f50b269",
+    "url": "/henri/static/js/1.fed5b92d.chunk.js"
+  },
+  {
+    "revision": "da761812f0e419334d0f",
+    "url": "/henri/static/css/main.a36c041c.chunk.css"
+  },
+  {
+    "revision": "33aeacf5a42b616a2f46b33abb4a26b5",
+    "url": "/henri/index.html"
+  }
+];
